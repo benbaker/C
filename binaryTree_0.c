af2c0673f187ebf0d0a0713e5d4715d11b6ec2d5
Ben Baker <bensemailaddress@icloud.com>
2013-12-10 16:18:31 -0600

    Update binaryTree_0.c

--- binaryTree_0.c
+++ binaryTree_0.c
@@ -9,7 +9,6 @@ typedef struct N{
 	struct N* right;
 }tree;
 
-
 tree * createNode( int data );
 tree * addBranch(tree * root, tree * branch);
 tree * buildTree( char *fileName );
@@ -22,37 +21,31 @@ int countLeafNodes(tree *);
 void printPreorder(tree * root);
 void printInorder(tree * root);
 
-
 int main( int argc, char * argv[] ) {
 
   if (argc != 2) { printf("Insufficient arguments"); return 0; } 
 
-
-
 	tree *root = buildTree( argv[1] );
 	if (!root) { printf("Unable to open the file %s\n", argv[1]); return 0; }
-
-    printf("\nTree nodes in pre order traversal:\n");
+	
+	printf("\nTree nodes in pre order traversal:\n");
 	printPreorder(root);
 	// printInorder(root);
-    printf("\n\n");
+	printf("\n\n");
+	
+	printf("Longest branch has the length: %d:\n", longestBranch(root));
+	printf("Number of leaves in the tree %d:\n", countLeafNodes(root));
+	printf("\n");
 
-    printf("Longest branch has the length: %d:\n", longestBranch(root));
-    printf("Number of leaves in the tree %d:\n", countLeafNodes(root));
-    printf("\n");
-
-	int findMe=getSearchParameters();
-	int found = searchTree(root, findMe);
+	int findMe = getSearchParameters();
+	int found  = searchTree(root, findMe);
+	
 	found ? printf("Number %d is present in the tree.\n", findMe):
 	printf("Number %d is not present in the tree.\n", findMe);
+	
 	printPreorder(root);
-    printf("\n");
-
-
+    	printf("\n");
 }
-
-
-
 
 tree * buildTree( char *fileName ){
 
@@ -68,10 +61,11 @@ tree * buildTree( char *fileName ){
 		int data;
 		fscanf(f, "%d", &data );
 		root = addBranch( root, createNode( data ) );
-
-	} fclose(f); return root;
+	} 
+	
+	fclose(f); 
+	return root;
 }
-
 
 tree * createNode( int data ) {
 
@@ -90,9 +84,7 @@ tree * addBranch(tree * root, tree * branch) {
 	(root->data  < branch->data) ?
 	(root->right   = addBranch(root->right, branch), root):
 	branch;
-
 }
-
 
 int countNodes(tree *branch ) {
 
@@ -106,16 +98,13 @@ int countNodes(tree *branch ) {
 
 int countLeafNodes(tree *node ) {
 
-  if(node == NULL)       
-    return 0;
-  if(node->left == NULL && node->right==NULL)      
-    return 1;            
-  else
-    return countLeafNodes(node->left)+
-           countLeafNodes(node->right);      
+	if(node == NULL)       
+		return 0;
+	if(node->left == NULL && node->right==NULL)      
+		return 1;            
+	else return countLeafNodes(node->left)
+		  + countLeafNodes(node->right);      
 }
-
-
 
 int longestBranch(tree *root ) {
 
@@ -132,20 +121,9 @@ int longestBranch(tree *root ) {
 
 }
 
-
-
-int max(int a, int b) {
-
-	return a > b ? a : b;
-
-}
-
-
-
-
+int max(int a, int b) { return a > b ? a : b; }
 
 int getSearchParameters() {
-
 	printf("Enter the number for which you want to search: ");
 	int findMe; scanf("%d", &findMe); return findMe;
 }
@@ -159,8 +137,8 @@ int searchTree( tree * node, int target ) {
 
 }
 
-
 void printPreorder(tree * root) {
+	
     if (root) {
         printf("%d ",root->data);
         printPreorder(root->left);
@@ -170,6 +148,7 @@ void printPreorder(tree * root) {
 }
 
 void printInorder(tree * root) {
+	
     if (root) {
         printInorder(root->left);
         printf("%d ",root->data);
